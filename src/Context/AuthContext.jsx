6f1f8daabbbadd7721b2e2428006abe1ab8fed5f
Midemee <ashiwajuaramide@gmail.com>
2025-09-08 12:45:53 +0100

    replaced endpoints with deployed link

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -14,7 +14,7 @@ export const AuthProvider= ({children})=>{
 
     const login= async (email, password) => {
         try{
-            const response = await fetch('http://localhost:5000/api/user/signin', {
+            const response = await fetch('https://plwcauth.onrender.com/api/user/signin', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -44,7 +44,7 @@ const handleSubmit = async (e) => {
   setSubmitError('');
   setLoading(true)
   try {
-    const response = await fetch(`http://localhost:5000/api/user/signup`, {
+    const response = await fetch(`https://plwcauth.onrender.com/api/user/signup`, {
             method: "POST",
             headers: { "Content-Type" : "application/json"},
             body: JSON.stringify(formData),
